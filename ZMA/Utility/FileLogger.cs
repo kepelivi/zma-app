d8ff48f83aab09faaa3e9f3cc4ecfd44038cfd1f
kepelivi <kepelivi7@gmail.com>
2024-04-23 16:19:28 +0200

    feat - create loggers

---
+++ ZMA/Utility/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿namespace ZMA.Utility;
+
+public class FileLogger : LoggerBase
+{
+    private readonly string _logFile;
+
+    public FileLogger(string logFile)
+    {
+        _logFile = logFile;
+    }
+
+    protected override void LogMessage(string message, string type)
+    {
+        var entry = CreateLogEntry(message, type);
+        using var streamWriter = File.AppendText(_logFile);
+        streamWriter.WriteLine(entry);
+        streamWriter.Close();
+    }
+}
---
+++ ZMA/Utility/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace ZMA.Utility;
+
+public interface ILogger
+{
+    public void LogInfo(string message);
+    public void LogError(string message);
+}
