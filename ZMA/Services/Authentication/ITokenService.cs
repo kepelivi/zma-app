c3b0c027dc0a3e873bb9775a1d4795ae552a5d90
kepelivi <kepelivi7@gmail.com>
2024-04-23 17:13:24 +0200

    feat - add authentication services

---
+++ ZMA/Services/Authentication/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ZMA.Services.Authentication;
+
+public interface ITokenService
+{
+    public string CreateToken(IdentityUser user, string role);
+}
---
+++ ZMA/Services/Authentication/TokenService.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ZMA.Services.Authentication;
+
+public class TokenService : ITokenService
+{
+    private const int ExpirationMinutes = 180;
+    private readonly IConfigurationRoot _config = new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .Build();
+
+    public string CreateToken(IdentityUser user, string role)
+    {
+        var expiration = DateTime.UtcNow.AddMinutes(ExpirationMinutes);
+        var token = CreateJwtToken(
+            CreateClaims(user, role),
+            CreateSigningCredentials(),
+            expiration
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    private JwtSecurityToken CreateJwtToken(List<Claim> claims, SigningCredentials credentials,
+        DateTime expiration) =>
+        new(
+            _config["ValidIssuer"] != null
+                ? _config["ValidIssuer"] : Environment.GetEnvironmentVariable("VALIDISSUER"),
+            _config["ValidAudience"] != null
+                ? _config["ValidAudience"] : Environment.GetEnvironmentVariable("VALIDAUDIENCE"),
+            claims,
+            expires: expiration,
+            signingCredentials: credentials
+        );
+
+    private List<Claim> CreateClaims(IdentityUser user, string? role)
+    {
+        try
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "TokenForTheApiWithAuth"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+            if (role != null)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["IssuerSigningKey"] != null ? _config["IssuerSigningKey"] : Environment.GetEnvironmentVariable("ISSUERSIGNINGKEY"))
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+}
