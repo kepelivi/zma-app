3a27f407258a208437a3482610f360e3ac984bcf
kepelivi <kepelivi7@gmail.com>
2024-04-27 14:49:29 +0200

    feat - create PartyController

---
+++ ZMA/Controllers/PartyController.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZMA.Model;
+using ZMA.Services.Repositories;
+using Host = ZMA.Model.Host;
+using ILogger = ZMA.Utility.ILogger;
+
+namespace ZMA.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PartyController(IPartyRepository partyRepository, UserManager<Host> userManager, ILogger logger) : ControllerBase
+{
+    [HttpGet("CreateParty"), Authorize(Roles = "Host")]
+    public async Task<ActionResult> CreateParty([Required] string name, string? details, [Required] string category, [Required] DateTime date)
+    {
+        try
+        {
+            var host = await userManager.Users.SingleAsync(h => User.Identity != null && h.UserName == User.Identity.Name);
+
+            var party = new Party {Name = name, Host = host, Date = date, Details = details, Category = category};
+            
+            partyRepository.CreateParty(party);
+
+            logger.LogInfo($"New party {party.Name} created at {DateTime.Now.ToShortDateString()} for the date of {party.Date}");
+            
+            return Ok(party);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e.Message);
+            return NotFound("Creating party failed.");
+        }
+    }
+}
