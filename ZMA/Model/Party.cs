9baef1e7c888aaf6350d66b1c470cb6a6efd75d8
kepelivi <kepelivi7@gmail.com>
2024-05-02 13:11:32 +0200

    change - int id to guid id

--- ZMA/Model/Party.cs
+++ ZMA/Model/Party.cs
@@ -2,7 +2,7 @@ namespace ZMA.Model;
 
 public class Party
 {
-    public int Id { get; init; }
+    public Guid Id { get; init; }
     public string Name { get; init; }
     public Host? Host { get; init; }
     public string? Details { get; init; }
