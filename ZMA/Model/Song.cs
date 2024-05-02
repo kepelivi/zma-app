773b451a346167e12e36a92a9e5494c3fd761718
kepelivi <kepelivi7@gmail.com>
2024-05-02 13:11:00 +0200

    change - configure properties

--- ZMA/Model/Song.cs
+++ ZMA/Model/Song.cs
@@ -3,8 +3,7 @@ namespace ZMA.Model;
 public class Song
 {
     public int Id { get; init; }
-    public string Title { get; init; }
-    public string Artist { get; init; }
+    public string? Title { get; init; }
     public DateTime RequestTime { get; init; }
-    public bool Accepted { get; init; }
+    public bool Accepted { get; set; }
 }
