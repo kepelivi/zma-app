d9de457b786f47bfe91a23b3fc4afde3c47b3132
kepelivi <kepelivi7@gmail.com>
2024-05-15 14:55:18 +0200

    feat - show all parties of the dj

--- ZMA-client/ZMA-app/app/party.js
+++ ZMA-client/ZMA-app/app/party.js
@@ -1,13 +1,16 @@
-import { StyleSheet, View, Text, FlatList, Pressable, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, FlatList, SafeAreaView } from 'react-native';
 import { useState, useEffect } from 'react';
+import { useNavigation } from "expo-router";
 
 import { COLORS } from '../constants/theme';
+import PartyCard from '../components/partyCard';
 
 import Logo from '../components/logo';
 
 export default function PartyManager() {
     const [parties, setParties] = useState([]);
     const [loading, setLoading] = useState(true);
+    const navigation = useNavigation();
 
     async function fetchParties() {
         const res = await fetch(`http://localhost:5086/Party/GetParties`,
@@ -31,26 +34,37 @@ export default function PartyManager() {
     if (loading) return <View><Text>Loading...</Text></View>
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.safeArea}>
             <Logo />
-            <View style={styles.container}>
+            <View style={styles.header}>
                 <Text style={styles.main}>Bulik</Text>
             </View>
             <FlatList
                 data={parties}
+                renderItem={({ item }) => <PartyCard party={item} onPress={() => navigation.navigate('songRequests', { id: item.id } )} />}
+                keyExtractor={item => item.id}
+                contentContainerStyle={styles.listContent}
             />
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS.white,
-        alignItems: 'center',
-        justifyContent: 'center',
+    safeArea: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    header: {
+      padding: 16,
+      backgroundColor: COLORS.deepPurple,
+      alignItems: 'center',
     },
     main: {
-        fontSize: 25
-    }
-});
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: '#fff',
+    },
+    listContent: {
+      padding: 16,
+    },
+  });
