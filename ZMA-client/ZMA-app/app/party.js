45d54f88dc92767fee4faccabb72e6bc9c1194d3
kepelivi <kepelivi7@gmail.com>
2024-06-23 18:16:33 +0200

    change - make fetch functions exportable in party.js for mocking purposes

--- ZMA-client/ZMA-app/app/party.js
+++ ZMA-client/ZMA-app/app/party.js
@@ -8,47 +8,47 @@ import Logo from '../components/logo';
 import Loading from '../components/loading';
 import LogOut from '../components/logout';
 
+export async function fetchParties() {
+    const res = await fetch(`http://localhost:5086/Party/GetParties`,
+        {
+            method: "GET",
+            credentials: 'include',
+            headers: { 'Content-type': 'application/json' }
+        }
+    );
+    return await res.json();
+}
+
+export async function fetchAndSortParties(setParties, setLoading) {
+    try {
+        const parties = await fetchParties();
+        const sortedParties = parties.sort((a, b) => new Date(a.date) - new Date(b.date));
+        setParties(sortedParties);
+    } catch (error) {
+        console.error('Failed to fetch and sort parties', error);
+    } finally {
+        setLoading(false);
+    }
+};
+
+export async function onDelete(id, setParties, setLoading) {
+    await fetch(`http://localhost:5086/Party/DeleteParty?partyId=${id}`,
+        {
+            method: "DELETE",
+            credentials: 'include',
+            headers: { 'Content-type': 'application/json' }
+        }
+    );
+    fetchAndSortParties(setParties, setLoading);
+}
+
 export default function PartyManager() {
     const [parties, setParties] = useState([]);
     const [loading, setLoading] = useState(true);
     const navigation = useNavigation();
 
-    async function fetchParties() {
-        const res = await fetch(`http://localhost:5086/Party/GetParties`,
-            {
-                method: "GET",
-                credentials: 'include',
-                headers: { 'Content-type': 'application/json' }
-            }
-        );
-        return await res.json();
-    }
-
-    async function onDelete(id) {
-        await fetch(`http://localhost:5086/Party/DeleteParty?partyId=${id}`,
-            {
-                method: "DELETE",
-                credentials: 'include',
-                headers: { 'Content-type': 'application/json' }
-            }
-        );
-        fetchAndSortParties();
-    }
-
-    async function fetchAndSortParties() {
-        try {
-            const parties = await fetchParties();
-            const sortedParties = parties.sort((a, b) => new Date(a.date) - new Date(b.date));
-            setParties(sortedParties);
-        } catch (error) {
-            console.error('Failed to fetch and sort parties', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
     useEffect(() => {
-        fetchAndSortParties();
+        fetchAndSortParties(setParties, setLoading);
     }, []);
 
     if (loading) return <Loading />
@@ -67,7 +67,7 @@ export default function PartyManager() {
             </View>
             <FlatList
                 data={parties}
-                renderItem={({ item }) => <PartyCard party={item} onPress={() => navigation.navigate('songRequests', { id: item.id })} onDelete={() => onDelete(item.id)} />}
+                renderItem={({ item }) => <PartyCard party={item} onPress={() => navigation.navigate('songRequests', { id: item.id })} onDelete={() => onDelete(item.id, setParties, setLoading)} />}
                 keyExtractor={item => item.id}
                 contentContainerStyle={styles.listContent}
             />
