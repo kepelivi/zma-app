1ec32fb2ea6bfff72b84de69bbfb1fd7db5c9a0d
kepelivi <kepelivi7@gmail.com>
2024-07-24 21:26:46 +0200

    remove - unnecessary imports

--- ZMA-client/ZMA-app/app/index.js
+++ ZMA-client/ZMA-app/app/index.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState, useEffect } from 'react';
-import { Link } from 'expo-router';
 
 //constants
 import {COLORS} from '../constants/theme';
