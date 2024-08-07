165270e8e47da07094b9da64dd28c6bad7978cc2
kepelivi <kepelivi7@gmail.com>
2024-08-07 17:06:52 +0200

    refactor - unit tests based on previous components changes

--- ZMA-client/ZMA-app/tests/unit-tests/logo.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import Logo from "../../components/logo";
-
-test('Logo should render correctly', () => {
-    const { getByText } = render(<Logo />);
-
-    expect(getByText("ZMA")).toBeTruthy();
-});
