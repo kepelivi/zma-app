362aaf4a33b971fcb470406bcf1eae4e63afd85f
kepelivi <kepelivi7@gmail.com>
2024-08-07 16:45:05 +0200

    add - new colors to theme

--- ZMA-client/ZMA-app/constants/theme.js
+++ ZMA-client/ZMA-app/constants/theme.js
@@ -1,9 +1,11 @@
 const COLORS = {
     purple: '#9c27b0',
-    deepPurple: "#3f0257",
+    deepPurple: "#301B3F",
     white: "#ffffff",
     black: "#000000",
-    greyish: "#cccccc"
+    greyish: "#3C415C",
+    accent: "#151515",
+    ashAndCreme: "#B4A5A5",
 }
 
 export { COLORS };
